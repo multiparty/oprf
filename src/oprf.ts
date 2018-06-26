b177c4ccbe0787248a95e67a175b982c77d33fa1
lucyq <lucyzqin@gmail.com>
2018-06-26 16:16:41 -0400

    more set up

--- src/oprf.ts
+++ src/oprf.ts
@@ -1,3 +1,16 @@
+import elliptic = require('elliptic');
+import BN = require('bn.js');
+
 export namespace OPRF{
-  
+
+  const curve = elliptic.ec;
+  const ec = new curve('curve25519');
+  const prime = (new BN(2)).pow(new BN(252)).add(new BN('27742317777372353535851937790883648493'));
+
+
+  export function randomizeInput(input: string) {
+    var mask = new Uint8Array(32);
+    window.crypto.getRandomValues(mask);
+  }
+
 }

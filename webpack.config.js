efd677a76961f40955115cd21786abbc890553a4
Frederick Jansen <frederick.jansen@gmail.com>
2018-07-02 18:33:28 -0400

    Add webpack and istanbul

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: "./src/oprf.ts",
+  output: {
+    filename: "bundle.js"
+  },
+  resolve: {
+    // Add '.ts' and '.tsx' as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      // all files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+};
